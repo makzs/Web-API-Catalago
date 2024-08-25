fb9e1929167af535969a352bd0bad69ee12aedf6
makzs <erikmakiak@gmail.com>
2024-08-25 16:01:52 -0300

    Add project files.

---
+++ APICatalago/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalago.Models
+{
+    [Table("Categoria")]
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? ImagemUrl { get; set; }
+
+
+        public ICollection<Produto>? Produtos { get; set; }
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+    }
+}
---
+++ APICatalago/Program.cs
@@ -0,0 +1,34 @@
+using APICatalago.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ignora o erro de ciclos (erro de repetição de um get entidade que faz referencia a outra entidade de forma ciclica)
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var SQLConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => 
+{
+    options.UseSqlServer(SQLConnection);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
